bc245ac10b131adedb7aa6cb8d9372d11a51cf4a
Hleb Kriukov <glebos05@gmail.com>
2020-12-19 21:00:14 +0200

    done

---
+++ ConvertToXML/Contact.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConvertToXML
+{
+    public class Contact
+    {
+        public string Name;
+        public string Number;
+    }
+}
---
+++ ConvertToXML/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConvertToXML
+{
+
+    class Program
+    {
+        const string path = "XmlSave";
+        static private List<Contact> contacts = new List<Contact>();
+        public static Contact AddContact(Contact contact)
+        {
+            contacts.Add(contact);
+            return contact;
+        }
+
+        public static void SaveToXml()
+        {
+            var XmlSerializer = new XmlSerializer(typeof(List<Contact>));
+            using(StreamWriter sw = new StreamWriter(path))
+            {
+                foreach(Contact c in contacts)
+                {
+                    XmlSerializer.Serialize(sw, contacts);
+                }
+            }
+
+        }
+
+        public static bool IsEnd = false;
+        static void Main(string[] args)
+        {
+            while (!IsEnd)
+            {
+                string InputComand = Console.ReadLine();
+                if(InputComand == "/add contact")
+                {
+                    Contact contact = new Contact();
+                    while (contact.Name == null || contact.Number == null) {
+                        string InputContact = Console.ReadLine();
+                        if (InputContact == "/add name")
+                        {
+                            Console.WriteLine("new name: ");
+                            string InputValue = Console.ReadLine();
+                            contact.Name = InputValue;
+                        }
+                        else if (InputContact == "/add number")
+                        {
+                            Console.WriteLine("new number: ");
+                            string InputValue = Console.ReadLine();
+                            contact.Number = InputValue;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Incorect command for contacts. Write /add number or /add name to do some with this information");
+                        }
+                    }
+                    AddContact(contact);
+                }
+                else if(InputComand == "/end")
+                {
+                    IsEnd = true;
+                }
+                else
+                {
+                    Console.WriteLine("pls write /add contact to add contact");
+                }
+                
+            }
+            SaveToXml();
+        }
+    }
+}
